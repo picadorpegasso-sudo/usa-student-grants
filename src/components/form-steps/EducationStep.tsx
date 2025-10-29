52449fa31c463df2c0f1d48523bb4bbcf4dec673
picadorpegasso-sudo <picadorpegasso@gmail.com>
2025-10-29 23:03:27 +0300

    Update EducationStep.tsx

--- src/components/form-steps/EducationStep.tsx
+++ src/components/form-steps/EducationStep.tsx
@@ -2,61 +2,39 @@ import { UseFormReturn } from "react-hook-form";
 import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { useState } from "react";
 
 interface EducationStepProps {
   form: UseFormReturn<any>;
 }
 
 export function EducationStep({ form }: EducationStepProps) {
+  const [isStudent, setIsStudent] = useState(true);
+
   return (
     <div className="space-y-4">
-      <FormField
-        control={form.control}
-        name="university"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Current University/College</FormLabel>
-            <FormControl>
-              <Input placeholder="Harvard University" {...field} />
-            </FormControl>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-
-      <FormField
-        control={form.control}
-        name="fieldOfStudy"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Field of Study</FormLabel>
-            <FormControl>
-              <Input placeholder="Computer Science" {...field} />
-            </FormControl>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-
       <FormField
         control={form.control}
         name="academicYear"
         render={({ field }) => (
           <FormItem>
-            <FormLabel>Academic Year</FormLabel>
-            <Select onValueChange={field.onChange} value={field.value}>
+            <FormLabel>Are you currently a student?</FormLabel>
+            <Select 
+              onValueChange={(value) => {
+                setIsStudent(value === "yes");
+                field.onChange(value);
+              }} 
+              value={field.value}
+            >
               <FormControl>
                 <SelectTrigger>
-                  <SelectValue placeholder="Select your year" />
+                  <SelectValue placeholder="Select an option" />
                 </SelectTrigger>
               </FormControl>
               <SelectContent>
-                <SelectItem value="freshman">Freshman (1st year)</SelectItem>
-                <SelectItem value="sophomore">Sophomore (2nd year)</SelectItem>
-                <SelectItem value="junior">Junior (3rd year)</SelectItem>
-                <SelectItem value="senior">Senior (4th year)</SelectItem>
-                <SelectItem value="graduate">Graduate Student</SelectItem>
-                <SelectItem value="other">Other</SelectItem>
+                <SelectItem value="yes">Yes, I'm a student</SelectItem>
+                <SelectItem value="no">No, I'm not currently studying</SelectItem>
+                <SelectItem value="planning">Planning to start studies soon</SelectItem>
               </SelectContent>
             </Select>
             <FormMessage />
@@ -64,31 +42,73 @@ export function EducationStep({ form }: EducationStepProps) {
         )}
       />
 
-      <FormField
-        control={form.control}
-        name="gpaRange"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>GPA Range</FormLabel>
-            <Select onValueChange={field.onChange} value={field.value}>
-              <FormControl>
-                <SelectTrigger>
-                  <SelectValue placeholder="Select your GPA range" />
-                </SelectTrigger>
-              </FormControl>
-              <SelectContent>
-                <SelectItem value="4.0">4.0 (Perfect)</SelectItem>
-                <SelectItem value="3.5-3.9">3.5 - 3.9</SelectItem>
-                <SelectItem value="3.0-3.4">3.0 - 3.4</SelectItem>
-                <SelectItem value="2.5-2.9">2.5 - 2.9</SelectItem>
-                <SelectItem value="2.0-2.4">2.0 - 2.4</SelectItem>
-                <SelectItem value="below-2.0">Below 2.0</SelectItem>
-              </SelectContent>
-            </Select>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
+      {isStudent && (
+        <>
+          <FormField
+            control={form.control}
+            name="university"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Current University/College</FormLabel>
+                <FormControl>
+                  <Input placeholder="Harvard University" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="fieldOfStudy"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Field of Study</FormLabel>
+                <FormControl>
+                  <Input placeholder="Computer Science" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="gpaRange"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>GPA Range (Optional)</FormLabel>
+                <Select onValueChange={field.onChange} value={field.value}>
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select your GPA range" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="4.0">4.0 (Perfect)</SelectItem>
+                    <SelectItem value="3.5-3.9">3.5 - 3.9</SelectItem>
+                    <SelectItem value="3.0-3.4">3.0 - 3.4</SelectItem>
+                    <SelectItem value="2.5-2.9">2.5 - 2.9</SelectItem>
+                    <SelectItem value="2.0-2.4">2.0 - 2.4</SelectItem>
+                    <SelectItem value="below-2.0">Below 2.0</SelectItem>
+                    <SelectItem value="not-applicable">Not Applicable</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </>
+      )}
+
+      {!isStudent && (
+        <div className="bg-blue-50 border border-blue-200 rounded-lg p-4">
+          <p className="text-blue-700 text-sm">
+            Even if you're not currently studying, you may still qualify for certain grants and financial aid programs 
+            for prospective students or career changers.
+          </p>
+        </div>
+      )}
     </div>
   );
 }
